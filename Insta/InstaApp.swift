d90d942e17f991c348b63dcdc407ce849af76305
crazyreleaser <crazyreleaser@yandex.ru>
2022-02-04 19:49:27 +0300

    Initial Commit

---
+++ Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by admin on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
